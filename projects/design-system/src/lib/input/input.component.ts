b3dcb6ed863999ffa06ce8b93cf8274dd2db788c
Stefan Haas <stefan.haas.privat@gmail.com>
2023-07-25 11:34:16 +0200

    updated input component

--- projects/design-system/src/lib/input/input.component.ts
+++ projects/design-system/src/lib/input/input.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -8,9 +14,12 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, FormsModule],
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputComponent {
-  @Input({required: true}) value!: string;
-  @Output() valueChange = new EventEmitter<string>();
+  @Input({ required: true }) value!: string | null;
+  @Input({ required: true }) label!: string;
+  @Input({ required: true }) id!: 'string';
+  @Input({ required: true }) placeholder!: string;
+  @Output() valueChange = new EventEmitter<string | null>();
 }
